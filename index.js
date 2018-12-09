c9e95ff0f54fcac1f04a8356fc9cc5661e2e94cf
Mehul Bechra <bechra.mehul@gmail.com>
2018-12-10 01:16:52 +0530

    Added cluster to prevent server crashing

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 
 // Dependencies
+const cluster = require('cluster');
+const cCPUs = require('os').cpus().length;
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
@@ -8,27 +10,41 @@ const index = require('./routes/index');
 const search = require('./routes/search');
 const list = require('./routes/list');
 
-// Connecting to databse
-mongoose.connect('mongodb://scraperuser:scraperuser12@ds217671.mlab.com:17671/scraper', { useNewUrlParser: true })
-  .then(() => console.log('Database connected'))
-  .catch(err => console.log('Could not connect to MongoDb'));
+if (cluster.isMaster) {
+  // Create a worker for each CPU
+  for (let i = 0; i < cCPUs; i++) {
+    cluster.fork();
+  }
 
-// Initializing express
-const app = express();
+  cluster.on('online', (worker) => {
+    console.log(`Worker ${worker.process.pid} is online.`);
+  });
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died.`);
+  });
+} else {
+  // Connecting to databse
+  mongoose.connect('mongodb://scraperuser:scraperuser12@ds217671.mlab.com:17671/scraper', { useNewUrlParser: true })
+    .then(() => console.log('Database connected'))
+    .catch(err => console.log('Could not connect to MongoDb'));
 
-// View Engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+  // Initializing express
+  const app = express();
 
-// Middleware
-app.use(express.json());
-app.use(express.static('public'));
+  // View Engine
+  app.set('view engine', 'ejs');
+  app.set('views', path.join(__dirname, 'views'));
 
-// Routes
-app.use('/', index);
-app.use('/list/', list);
-app.use('/api/search/', search);
+  // Middleware
+  app.use(express.json());
+  app.use(express.static('public'));
 
-// Start server
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+  // Routes
+  app.use('/', index);
+  app.use('/list/', list);
+  app.use('/api/search/', search);
+
+  // Start server
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`Listening on port ${port}...`));
+}
